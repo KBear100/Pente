2beb89d4874804365bcb833c0cbac87122363c01
enicewanner <enicewanner@student.neumont.edu>
2024-02-02 20:26:03 -0700

    The Logic Update Pt. 1
    Added win condition logic.
    Added Player class and Board class.
    Know Issues: Top rows can result in index out of bounds error. Lines can loop map to win.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,37 +16,141 @@ namespace Pente
     /// </summary>
     public partial class MainWindow : Window
     {
-        int yes = 0;
+        int currentIndex = 0;
         int x = 0;
         int y = 0;
 
         int player = 1;
+        Player player1 = new Player(Brushes.Red);
+        Player player2 = new Player(Brushes.Blue);
+        Player currentPlayer;
 
         public MainWindow()
         {
             InitializeComponent();
+            currentPlayer = player1;
         }
         public void On_Click(object sender, RoutedEventArgs e)
         {
-            yes = Board_Grid.Children.IndexOf(sender as Button);
+            currentIndex = Board_Grid.Children.IndexOf(sender as Button);
 
-            int x = yes % 19 + 1;
-            int y = yes / 19 + 1;
+            int x = currentIndex % 19 + 1;
+            int y = currentIndex / 19 + 1;
 
-            if (player == 1)
+            if (currentPlayer == player1)
             {
-                (sender as Button).Background = Brushes.Red;
+                (sender as Button).Background = currentPlayer.playerColor;
                 (sender as Button).Click -= On_Click;
-                player = 2;
+                if (Check_Win(currentIndex)) 
+                {
+                    MessageBox.Show("Player 1 Win");
+                }
+                else currentPlayer = player2;
             }
-            else if (player == 2)
+            else if (currentPlayer == player2)
             {
-                (sender as Button).Background= Brushes.Blue;
+                (sender as Button).Background = currentPlayer.playerColor;
                 (sender as Button).Click -= On_Click;
-                player = 1;
+                if (Check_Win(currentIndex))
+                {
+                    MessageBox.Show("Player 2 Win");
+                }
+                else currentPlayer = player1;
 
             }
         }
+
+        public bool Check_Win(int SelectedIndex)
+        {
+            if (Check_Horizontal(SelectedIndex) || (Check_Vertical(SelectedIndex)) || Check_Diagonals(SelectedIndex))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Check_Horizontal(int SelectedIndex)
+        {
+            int count = 1;
+
+            for (int i = 1; count <= 5; i++)
+            {
+                if ((Board_Grid.Children[SelectedIndex + i] as Button).Background == currentPlayer.playerColor) count++;
+                else break;
+            }
+
+            for (int i = 1; count <= 5; i++)
+            {
+                if ((Board_Grid.Children[SelectedIndex - i] as Button).Background == currentPlayer.playerColor) count++;
+                else break;
+            }
+
+            if (count == 3) MessageBox.Show("Three");
+            if (count == 4) MessageBox.Show("Four");
+            if (count == 5) return true;
+            return false;
+        }
+
+        public bool Check_Vertical(int SelectedIndex) 
+        {
+            int count = 1;
+
+            for (int i = 1; count <= 5; i++)
+            {
+                if ((Board_Grid.Children[SelectedIndex + (19 * i)] as Button).Background == currentPlayer.playerColor) count++;
+                else break;
+            }
+
+            for (int i = 1; count <= 5; i++)
+            {
+                if ((Board_Grid.Children[SelectedIndex - (19 * i)] as Button).Background == currentPlayer.playerColor) count++;
+                else break;
+            }
+
+            if (count == 5) return true;
+
+            if (count == 3) MessageBox.Show("Three");
+            if (count == 4) MessageBox.Show("Four");
+            return false;
+        }
+
+        public bool Check_Diagonals(int SelectedIndex)
+        {
+            int count1 = 1;
+            int count2 = 1;
+
+            for (int i = 1; count1 <= 5; i++)
+            {
+                if ((Board_Grid.Children[SelectedIndex + (19 * i) + i] as Button).Background == currentPlayer.playerColor) count1++;
+                else break;
+            }
+
+            for (int i = 1; count1 <= 5; i++)
+            {
+                if ((Board_Grid.Children[SelectedIndex - (19 * i) - i] as Button).Background == currentPlayer.playerColor) count1++;
+                else break;
+            }
+
+            for (int i = 1; count2 <= 5; i++)
+            {
+                if ((Board_Grid.Children[SelectedIndex + (19 * i) - i] as Button).Background == currentPlayer.playerColor) count2++;
+                else break;
+            }
+
+            for (int i = 1; count2 <= 5; i++)
+            {
+                if ((Board_Grid.Children[SelectedIndex - (19 * i) + i] as Button).Background == currentPlayer.playerColor) count2++;
+                else break;
+            }
+
+            if (count1 == 3 || count2 == 3) MessageBox.Show("Three");
+            if (count2 == 4 || count2 == 4) MessageBox.Show("Four");
+            if (count1 == 5 || count2 == 5) return true;
+
+            return false;
+        }
+
+
     }
 
 
---
+++ Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Pente
+{
+    class Player
+    {
+        public Player(Brush playerColor, string playerName = "Player")
+        {
+            this.playerColor = playerColor;
+            this.playerName = playerName;
+        }
+
+        public Brush playerColor;
+        public string playerName;
+
+    }
+}
